dfdd529a6178db76c69be073c66999d373ceab75
Mayank Waikar <mayank.waikar@gmail.com>
2021-07-13 00:12:02 +0530

    initial commit

---
+++ employeemanager/src/main/java/com/practice/crud/employeemanager/model/Employee.java
@@ -0,0 +1,13 @@
+package com.practice.crud.employeemanager.model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+}
